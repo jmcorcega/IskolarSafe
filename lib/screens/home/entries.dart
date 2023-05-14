4f945108c7f8e62b9a64ec93063fad3e9d9f1367
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-14 20:25:02 +0800

    feat: Add a basic 'entries' dashboard
    Do the initial FAB, as well as the top menu for logging out.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- lib/screens/home/entries.dart
+++ lib/screens/home/entries.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class Entries extends StatefulWidget {
   static const String routeName = "/";
@@ -10,11 +11,47 @@ class Entries extends StatefulWidget {
 }
 
 class _EntriesState extends State<Entries> {
+  List<IconData> _listIcons = [Symbols.login_rounded, Symbols.logout_rounded];
+  List<String> _listStrings = ["My Account", "Logout"];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      'Learn 📗',
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("My Entries"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showMenu(
+                  context: context,
+                  position: const RelativeRect.fromLTRB(100, 20, 0, 100),
+                  items: List.generate(_listIcons.length, (index) {
+                    return PopupMenuItem(
+                        value: index,
+                        onTap: () {},
+                        child: Wrap(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Icon(_listIcons[index]),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Text(_listStrings[index]))
+                          ],
+                        ));
+                  }));
+            },
+            icon: const Icon(Symbols.more_vert_rounded),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text("My Profile"),
+        icon: const Icon(Symbols.person_filled_rounded),
+      ),
+    );
   }
 }
