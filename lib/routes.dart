1d8e7d7e75b0e277de5f0c163814fc313448965d
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-15 15:03:51 +0800

    refactor: Redesign main login screen
    Move back the email and password to the main login 'welcome' screen and add the Google login button there, completely removing the separate email login screen.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:iskolarsafe/screens/home.dart';
 import 'package:iskolarsafe/screens/login.dart';
-import 'package:iskolarsafe/screens/login/email.dart';
 
 class IskolarSafeRoutes {
   static Map<String, Widget Function(BuildContext)> routes = {
     Home.routeName: (context) => const Home(),
     Login.routeName: (context) => const Login(),
-    LoginViaEmail.routeName: (context) => const LoginViaEmail(),
   };
 
   static Route<dynamic>? dynamicRouteHandler(RouteSettings settings) {
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,52 +1,189 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:iskolarsafe/screens/login/email.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   static const String routeName = "/login";
-  static const Size _buttonSize = Size(225.0, 50.0);
 
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _loginErr = false;
+  bool _loadingButton = false;
+
+  static const Size _buttonSize = Size(225.0, 47.5);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const FlutterLogo(size: 90),
-            const SizedBox(height: 24.0),
-            Text("Welcome to IskolarSafe",
-                style: Theme.of(context).textTheme.headlineSmall),
-            const SizedBox(height: 72.0),
-            FilledButton.icon(
-              style: FilledButton.styleFrom(minimumSize: _buttonSize),
-              onPressed: () {},
-              icon: const Icon(
-                Bootstrap.google,
-                size: 18.0,
+            Expanded(
+              flex: 1,
+              child: Text("IskolarSafe",
+                  style: Theme.of(context).textTheme.titleLarge!.apply(
+                        fontWeightDelta: 1,
+                        fontSizeDelta: 4,
+                      )),
+            ),
+            Expanded(
+              flex: 9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListView(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      Text(
+                        "Login to your account",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .displaySmall!
+                            .apply(fontSizeDelta: -8),
+                      ),
+                      const SizedBox(height: 48.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: emailController,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Symbols.email_rounded),
+                                  border: OutlineInputBorder(),
+                                  labelText: "Email",
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Email is required.';
+                                  } else if (!EmailValidator.validate(value)) {
+                                    return 'Email is invalid.';
+                                  } else if (_loginErr) {
+                                    return 'Email may be incorrect.';
+                                  }
+
+                                  return null;
+                                },
+                              ),
+                              const SizedBox(height: 18.0),
+                              TextFormField(
+                                controller: passwordController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  prefixIcon: Icon(Symbols.key_rounded),
+                                  border: OutlineInputBorder(),
+                                  labelText: "Password",
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Password is required.';
+                                  } else if (value.length < 6) {
+                                    return 'Password must be at least 6 characters long.';
+                                  } else if (_loginErr) {
+                                    return 'Password may be incorrect.';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
+                          )),
+                      const SizedBox(height: 14.0),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            minimumSize: const Size(280.0, 20.0)),
+                        onPressed: () {},
+                        child: const Text("Forgot your password?"),
+                      ),
+                      const SizedBox(height: 14.0),
+                      FilledButton(
+                        style: FilledButton.styleFrom(minimumSize: _buttonSize),
+                        onPressed: _loadingButton
+                            ? null
+                            : () async {
+                                _loginErr = false;
+                                if (_formKey.currentState!.validate()) {
+                                  setState(() {
+                                    _loadingButton = true;
+                                  });
+
+                                  if (context.mounted) {
+                                    if (_loginErr) {
+                                      _formKey.currentState!.validate();
+                                      setState(() {
+                                        _loadingButton = false;
+                                      });
+                                    }
+                                  }
+                                }
+
+                                setState(() {
+                                  _loadingButton = false;
+                                });
+                              },
+                        child: _loadingButton
+                            ? Transform.scale(
+                                scale: 0.5,
+                                child: const CircularProgressIndicator(),
+                              )
+                            : const Text('Log in via email'),
+                      ),
+                      const SizedBox(height: 14.0),
+                      OutlinedButton.icon(
+                        style:
+                            OutlinedButton.styleFrom(minimumSize: _buttonSize),
+                        onPressed: () {
+                          //Navigator.pushNamed(context, LoginViaEmail.routeName);
+                        },
+                        icon: const Icon(
+                          Bootstrap.google,
+                          size: 18.0,
+                        ),
+                        label: const Text("Log in via Google"),
+                      ),
+                      const SizedBox(height: 48.0),
+                      TextButton.icon(
+                        style: TextButton.styleFrom(
+                            minimumSize: const Size(280.0, 40.0)),
+                        onPressed: () {},
+                        icon: const Icon(Symbols.person_add_rounded),
+                        label: const Text("Don't have an account yet?"),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              label: const Text("Log in via Google"),
             ),
-            const SizedBox(height: 18.0),
-            OutlinedButton.icon(
-              style: OutlinedButton.styleFrom(minimumSize: _buttonSize),
-              onPressed: () {
-                Navigator.pushNamed(context, LoginViaEmail.routeName);
-              },
-              icon: const Icon(Symbols.email_rounded),
-              label: const Text("Log in via Email"),
-            ),
-            const SizedBox(height: 140.0),
-            TextButton.icon(
-              style: TextButton.styleFrom(minimumSize: const Size(240.0, 40.0)),
-              onPressed: () {},
-              icon: const Icon(Symbols.person_add_rounded),
-              label: const Text("Don't have an account yet?"),
-            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  children: [
+                    Text(
+                        textAlign: TextAlign.center,
+                        "By using IskolarSafe, you agree to the app's terms and conditions and privacy policy.",
+                        style: Theme.of(context).textTheme.labelSmall)
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
