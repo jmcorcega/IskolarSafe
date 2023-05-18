286ab97ddf4f3c45d13bb675e710d2c42b925596
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-18 13:17:03 +0800

    feat: Add account and application menu
    Actually hash the application menu and add 'account' and app options from this, similar to how Google apps do their account switcher and app settings.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- lib/screens/home/entries.dart
+++ lib/screens/home/entries.dart
@@ -12,8 +12,157 @@ class Entries extends StatefulWidget {
 }
 
 class _EntriesState extends State<Entries> {
-  List<IconData> _listIcons = [Symbols.login_rounded, Symbols.logout_rounded];
-  List<String> _listStrings = ["My Account", "Logout"];
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) => AlertDialog(
+        alignment: Alignment.topCenter,
+        insetPadding: EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: MediaQuery.of(context).size.height * 0.085,
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(24.0)),
+        ),
+        contentPadding: const EdgeInsets.all(0.0),
+        content: Builder(
+          builder: (context) {
+            return SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 10.0),
+                  Row(
+                    children: [
+                      const SizedBox(width: 18.0),
+                      IconButton(
+                        icon: const Icon(Symbols.close_rounded),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      Expanded(
+                        child: Text(
+                          "IskolarSafe",
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.titleLarge!.apply(
+                                fontWeightDelta: 2,
+                                fontSizeDelta: -4,
+                              ),
+                        ),
+                      ),
+                      const IconButton(
+                        icon: Icon(null),
+                        onPressed: null,
+                      ),
+                      const SizedBox(width: 18.0),
+                    ],
+                  ),
+                  const SizedBox(height: 8.0),
+                  Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 12.0),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18.0)),
+                    ),
+                    elevation: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 12.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const SizedBox(height: 4.0),
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.primary,
+                                child: Text("U",
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary)),
+                              ),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'John Doe',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelLarge!
+                                          .apply(fontWeightDelta: 1),
+                                    ),
+                                    Text('johndoe@example.com',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelMedium),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 18.0),
+                          OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            child: const Text("Edit your profile"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  ListTile(
+                    leading: const Icon(Symbols.settings_rounded),
+                    title: const Text("App Settings"),
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 24.0),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Symbols.logout_rounded),
+                    title: const Text("Log out"),
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 24.0),
+                    onTap: () {
+                      // TODO: Should trigger logout
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, Login.routeName);
+                    },
+                  ),
+                  const SizedBox(height: 12.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          textAlign: TextAlign.center,
+                          "Stay safe, isko/iska!",
+                          style: Theme.of(context).textTheme.labelSmall,
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 12.0),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,34 +171,16 @@ class _EntriesState extends State<Entries> {
         centerTitle: true,
         title: const Text("My Entries"),
         actions: [
-          PopupMenuButton(
-              icon: const Icon(Symbols.more_vert_rounded),
-              onSelected: (result) {
-                switch (result) {
-                  case 0:
-                    // TODO: Should trigger logout
-                    Navigator.pushNamed(context, Login.routeName);
-                    break;
-                }
-              },
-              itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(
-                    value: 0,
-                    child: Wrap(
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Icon(Symbols.logout_rounded),
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(left: 20),
-                            child: Text("Logout"))
-                      ],
-                    ),
-                  ),
-                ];
-              }),
+          IconButton(
+            icon: CircleAvatar(
+              radius: 14,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: Text("U",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary)),
+            ),
+            onPressed: () => _dialogBuilder(context),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -67,6 +198,7 @@ class _EntriesState extends State<Entries> {
       context: context,
       isScrollControlled: true,
       builder: (context) => DraggableScrollableSheet(
+          snap: true,
           initialChildSize: 0.45,
           maxChildSize: 0.95,
           minChildSize: 0.4,
@@ -92,6 +224,23 @@ class ProfileModal extends StatelessWidget {
         FlutterLogo(size: 150.0),
         SizedBox(height: 18.0),
         Text("User's Name", style: Theme.of(context).textTheme.titleLarge),
+        TextButton.icon(
+          onPressed: null,
+          icon: const Icon(Symbols.check_circle_filled, size: 20.0),
+          label: Text("Safe for entry",
+              style: Theme.of(context).textTheme.labelLarge!.apply(
+                    fontSizeDelta: 1,
+                    color: Colors.green,
+                  )),
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 18.0, horizontal: 0.0),
+            ),
+            foregroundColor: MaterialStateProperty.all(
+              Colors.green,
+            ),
+          ),
+        ),
       ],
     );
   }
