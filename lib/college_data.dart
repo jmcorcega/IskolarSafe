003a0d2d38a430ec203562ccd033b5a3c1e4fdd9
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-21 18:04:16 +0800

    chore: Add college data class
    This is where we will store immutable but updateable data such as the colleges selection from sign up.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

---
+++ lib/college_data.dart
@@ -0,0 +1,17 @@
+class CollegeData {
+  static List<String> get colleges => _colleges;
+
+  static final List<String> _colleges = [
+    "College of Agriculture and Food Science",
+    "College of Arts and Sciences",
+    "College of Development Communication",
+    "College of Economics and Management",
+    "College of Engineering and Agro-Industrial Technology",
+    "School of Environmental Science and Management",
+    "College of Forestry and Natural Resources",
+    "Graduate School",
+    "College of Human Ecology",
+    "College of Public Affairs and Development",
+    "College of Veterinary Medicine"
+  ];
+}
