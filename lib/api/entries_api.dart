863d38d068c8fd1329bac14c72c70bd4564f80ac
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-05-27 17:26:54 +0800

    feat: Introduce initial health entry capabilities
    This can now add and list health entries.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

---
+++ lib/api/entries_api.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_catch_clause
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:iskolarsafe/models/user_model.dart';
+
+class HealthEntriesAPI {
+  static final FirebaseFirestore store = FirebaseFirestore.instance;
+  static const String _storeName = "entries";
+
+  // Get all entries from the database
+  Stream<QuerySnapshot> getAllEntries(IskolarInfo userInfo) {
+    // Students can only access their own entries, which
+    // requires special handling for Cloud Firestore
+    if (userInfo.type == IskolarType.student) {
+      return store
+          .collection(_storeName)
+          .where('userId', isEqualTo: userInfo.id)
+          .orderBy('dateGenerated', descending: true)
+          .snapshots();
+    }
+
+    return store.collection(_storeName).snapshots();
+  }
+
+  // Add entry to the firestore
+  Future<bool> uploadEntry(Map<String, dynamic> entry) async {
+    try {
+      final ref = await store.collection(_storeName).add(entry);
+      await store.collection(_storeName).doc(ref.id).update({'id': ref.id});
+
+      return true;
+    } on FirebaseException catch (e) {
+      return false;
+    }
+  }
+}
---
+++ lib/providers/entries_provider.dart
@@ -0,0 +1,45 @@
+/*
+  Based on work by Ms. Claizel Coubeili Cepe
+*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:iskolarsafe/api/entries_api.dart';
+import 'package:iskolarsafe/models/entry_model.dart';
+import 'package:iskolarsafe/models/user_model.dart';
+import 'package:iskolarsafe/providers/accounts_provider.dart';
+
+class HealthEntryProvider with ChangeNotifier {
+  /// Instance of [HealthEntriesAPI] used to interact with the Firestore database.
+  late HealthEntriesAPI api;
+
+  /// Stream of all health entries in the database.
+  late Stream<QuerySnapshot> _entryStream;
+  late IskolarInfo? _userInfo;
+
+  HealthEntryProvider() {
+    api = HealthEntriesAPI();
+    fetchEntries();
+  }
+
+  // Getters
+  Stream<QuerySnapshot> get entries => _entryStream;
+
+  /// Fetches all health entries from the database.
+  fetchEntries() async {
+    _userInfo = await AccountsProvider().userInfo;
+    _entryStream = api.getAllEntries(_userInfo!);
+    notifyListeners();
+  }
+
+  Future<void> refetchEntries() async {
+    await fetchEntries();
+    notifyListeners();
+  }
+
+  /// Adds a new slam book entry to the database.
+  void addEntry(HealthEntry entry) async {
+    await api.uploadEntry(HealthEntry.toJson(entry));
+    notifyListeners();
+  }
+}
