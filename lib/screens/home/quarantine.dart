8329a306eb72495a78742312fe5b125d78fc21f3
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-06-03 08:40:02 +0800

    chore: Remove unnecessary Column on Quarantine screen
    All of it is just a StreamBuilder now. No need for this. Remove the if-else statement checking if a student is quarantined as well, all the stream returns are all quarantined users at this point.
    
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- lib/screens/home/quarantine.dart
+++ lib/screens/home/quarantine.dart
@@ -2,7 +2,6 @@
 // DATE QUARANTINED
 // Remove Quarantine -> When clicked, show pop up
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:iskolarsafe/components/app_options.dart';
 import 'package:iskolarsafe/components/appbar_header.dart';
@@ -10,7 +9,6 @@ import 'package:iskolarsafe/components/health_confirm_dialog.dart';
 import 'package:iskolarsafe/components/requests_button.dart';
 import 'package:iskolarsafe/components/screen_placeholder.dart';
 import 'package:iskolarsafe/components/user_details.dart';
-import 'package:iskolarsafe/dummy_info.dart';
 import 'package:iskolarsafe/providers/accounts_provider.dart';
 import 'package:iskolarsafe/models/user_model.dart';
 import 'package:material_symbols_icons/symbols.dart';
@@ -28,84 +26,73 @@ class Quarantine extends StatefulWidget {
 class _QuarantineState extends State<Quarantine> {
   @override
   Widget build(BuildContext context) {
-    Stream<QuerySnapshot> students =
-        context.watch<AccountsProvider>().quarantined;
     return Scaffold(
-        appBar: AppBar(
-          leading: EditRequestButton(),
-          centerTitle: true,
-          title: const AppBarHeader(
-            icon: Symbols.medical_mask_rounded,
-            title: "Under Quarantine",
-            hasAction: false,
-          ),
-          actions: const [
-            AppOptions(),
-          ],
+      appBar: AppBar(
+        leading: EditRequestButton(),
+        centerTitle: true,
+        title: const AppBarHeader(
+          icon: Symbols.medical_mask_rounded,
+          title: "Under Quarantine",
+          hasAction: false,
         ),
-        body: Column(
-          children: [
-            StreamBuilder(
-                stream: students,
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(
-                        child: Text("Error encountered ${snapshot.error}"));
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Expanded(
-                      child: ScreenPlaceholder(
-                        asset: "assets/images/illust_no_quarantine.svg",
-                        text: "No users under quarantine",
-                      ),
-                    );
-                  }
+        actions: const [
+          AppOptions(),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: context.watch<AccountsProvider>().quarantined,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("Error encountered ${snapshot.error}"));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Expanded(
+              child: ScreenPlaceholder(
+                asset: "assets/images/illust_no_quarantine.svg",
+                text: "No users under quarantine",
+              ),
+            );
+          }
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data?.docs.length,
-                    itemBuilder: ((context, index) {
-                      IskolarInfo user = IskolarInfo.fromJson(
-                          snapshot.data?.docs[index].data()
-                              as Map<String, dynamic>);
-                      user.id = snapshot.data?.docs[index].id;
-                      if (user.status == IskolarHealthStatus.quarantined) {
-                        return ListTile(
-                          contentPadding:
-                              const EdgeInsets.symmetric(horizontal: 24.0),
-                          leading: _getHealthStatus(
-                              user.status != IskolarHealthStatus.healthy),
-                          title: Text("${user.firstName} ${user.lastName}"),
-                          subtitle: Text(
-                            "Quarantined on: 18/05/2023 11:53am",
-                            style: Theme.of(context).textTheme.labelMedium,
-                          ),
-                          trailing: SizedBox(
-                            width: 34.0,
-                            child: OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(0),
-                              ),
-                              onPressed: () =>
-                                  HealthConfirmDialog.confirmDialog(
-                                      context: context,
-                                      user: user,
-                                      type: HealthConfirmDialogType
-                                          .endQuarantine),
-                              child: const Icon(Symbols.close),
-                            ),
-                          ),
-                          onTap: () => UserDetails.showSheet(context, user),
-                        );
-                      }
-                    }),
-                  );
-                })
-          ],
-        ));
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: ((context, index) {
+              IskolarInfo user = IskolarInfo.fromJson(
+                  snapshot.data?.docs[index].data() as Map<String, dynamic>);
+              user.id = snapshot.data?.docs[index].id;
+
+              return ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+                leading: _getHealthStatus(
+                    user.status != IskolarHealthStatus.healthy),
+                title: Text("${user.firstName} ${user.lastName}"),
+                subtitle: Text(
+                  "Quarantined on: 18/05/2023 11:53am",
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+                trailing: SizedBox(
+                  width: 34.0,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(0),
+                    ),
+                    onPressed: () => HealthConfirmDialog.confirmDialog(
+                        context: context,
+                        user: user,
+                        type: HealthConfirmDialogType.endQuarantine),
+                    child: const Icon(Symbols.close),
+                  ),
+                ),
+                onTap: () => UserDetails.showSheet(context, user),
+              );
+            }),
+          );
+        },
+      ),
+    );
   }
 
   Widget _getHealthStatus(bool status) {
