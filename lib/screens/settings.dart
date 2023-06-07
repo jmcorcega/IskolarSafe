cc5b615fa636349055aecba77197806a268f450d
John Vincent Corcega <jmcorcega@up.edu.ph>
2023-06-07 21:47:55 +0800

    feat: Add about page
    Signed-off-by: John Vincent Corcega <jmcorcega@up.edu.ph>

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:iskolarsafe/components/appbar_header.dart';
@@ -31,50 +31,134 @@ class _SettingsState extends State<Settings> {
       ),
       body: ListView(
         children: [
-          const SizedBox(height: 16.0),
-          ListTile(
-            leading: Icon(Symbols.dark_mode_rounded),
-            title: Text("Dark mode"),
-            trailing: Switch(
-              value: _switchValue,
-              onChanged: (value) => {
-                setState(() {
-                  _switchValue = value;
-                  if (_switchValue == true) {
-                    //
-                  }
-                })
-              },
+          // const SizedBox(height: 16.0),
+          // ListTile(
+          //   leading: Icon(Symbols.dark_mode_rounded),
+          //   title: Text("Dark mode"),
+          //   trailing: Switch(
+          //     value: _switchValue,
+          //     onChanged: (value) => {
+          //       setState(() {
+          //         _switchValue = value;
+          //         if (_switchValue == true) {
+          //           //
+          //         }
+          //       })
+          //     },
+          //   ),
+          // ),
+          // const Divider(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+            child: Text(
+              "About IskolarSafe",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 4.0),
+            child: Text(
+              "Developers",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .apply(fontSizeDelta: -2, fontWeightDelta: 1),
             ),
           ),
           const Divider(),
           ListTile(
-            leading: Icon(Symbols.info_rounded),
-            title: Text("About App"),
-            onTap: () {
-              showDialog(
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (context) => AlertDialog(
-                      alignment: Alignment.topCenter,
-                      insetPadding: EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                        vertical: MediaQuery.of(context).size.height * 0.085,
-                      ),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(24.0)),
-                      ),
-                      contentPadding: const EdgeInsets.all(0.0),
-                      content: Builder(builder: (context) {
-                        return SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            child: Column(
-                              children: [Text("lalala")],
-                            ));
-                      })));
-            },
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.person_2_rounded),
+            title: Text("Kimberly M. Bandillo"),
+            subtitle: Text("Entry editing and scanning, UI scaffolding"),
           ),
           const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.person_rounded),
+            title: Text("John Vincent M. Corcega"),
+            subtitle: Text("Lead Developer, Firebase, Authentication"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.person_4_rounded),
+            title: Text("Angelo Kenzo F. Fabregas"),
+            subtitle: Text("Search, Application models, UI scaffolding"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.person_3_rounded),
+            title: Text("May R. Laban"),
+            subtitle: Text("Lead UI/UX, Entry requests, User profiles"),
+          ),
+          const Divider(),
+
+          const SizedBox(height: 24.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 4.0),
+            child: Text(
+              "Open-source Components",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .apply(fontSizeDelta: -2, fontWeightDelta: 1),
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.palette_rounded),
+            title: Text("Material Design and Components"),
+            subtitle: Text("Google"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.image_rounded),
+            title: Text("cached_network_image"),
+            subtitle: Text("baseflow.com"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.timer_rounded),
+            title: Text("relative_time"),
+            subtitle: Text("viter.nl"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.document_scanner_rounded),
+            title: Text("flutter_svg"),
+            subtitle: Text("dnfield.dev"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.data_alert_rounded),
+            title: Text("fluttertoast"),
+            subtitle: Text("karthikponnam.dev"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.image_aspect_ratio_rounded),
+            title: Text("svg_provider_cache"),
+            subtitle: Text("ho-doan"),
+          ),
+          const Divider(),
+          ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+            leading: Icon(Symbols.web_asset_rounded),
+            title: Text("scroll_shadow_container"),
+            subtitle: Text("nuc134r.io"),
+          ),
+          const Divider(),
+
+          const SizedBox(height: 24.0),
         ],
       ),
     );
